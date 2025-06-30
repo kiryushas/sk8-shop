330f4dc8690aec18fe0a620b9782ff320ed88969
kiryushas <kirilsvystun@gmail.com>
2025-06-30 22:20:32 +0200

    Add image support and layout changes

--- shop/models.py
+++ shop/models.py
@@ -10,6 +10,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     category = models.CharField(max_length=10, choices=CATEGORY_CHOICES)
+    image = models.ImageField(upload_to='product_images/', blank=True, null=True)  # ← ДОБАВЛЕНО
 
     def __str__(self):
         return self.name
