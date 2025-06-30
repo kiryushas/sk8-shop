2303becf95d1ddfde44caf4b04232b4c55d13de7
kiryushas <kirilsvystun@gmail.com>
2025-06-30 23:47:32 +0200

    Fix urls_auth syntax error

--- shop/urls_auth.py
+++ shop/urls_auth.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -6,13 +6,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='shop/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='product_list'), name='logout'),
     path('register/', views.register, name='register'),
-    
-    from django.urls import path, include
-
-urlpatterns = [
-    # ... ваши кастомные пути login, logout, register
 
     # Подключаем стандартные пути Django для аутентификации
     path('accounts/', include('django.contrib.auth.urls')),
-]
 ]
