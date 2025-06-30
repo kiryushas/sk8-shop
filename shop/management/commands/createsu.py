1d9f10207f58ec17fc4ca9181023502073bd2d17
kiryushas <kirilsvystun@gmail.com>
2025-06-30 21:33:46 +0200

    обновлён дизайн product_list

---
+++ shop/management/commands/createsu.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+    help = 'Create a superuser if none exists'
+
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+        if not User.objects.filter(is_superuser=True).exists():
+            User.objects.create_superuser(
+                username='admin',
+                email='admin@example.com',
+                password='admin123'
+            )
+            self.stdout.write(self.style.SUCCESS('Суперпользователь успешно создан!'))
+        else:
+            self.stdout.write('Суперпользователь уже существует.')
