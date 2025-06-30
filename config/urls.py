5825509d126e0b0f6237928d55350fbc3bbd0696
kiryushas <kirilsvystun@gmail.com>
2025-06-30 22:34:40 +0200

    fix: корректное подключение media в urls.py

--- config/urls.py
+++ config/urls.py
@@ -5,10 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('shop.urls')),
+    path('', include('shop.urls')),  # Основное приложение
 ]
 
-# Для обслуживания статики и медиа в режиме DEBUG
+# Подключение медиа и статики только в режиме отладки (DEBUG = True)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
